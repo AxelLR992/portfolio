29e28f615d9bc81966575ee1c57b7fc587f04d7e
Axel León <yka992@hotmail.com>
2022-02-22 14:52:20 -0600

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,20 @@
+/** @type {import('gatsby).GatsbyConfig} */
+module.exports = {
+  siteMetadata: {
+      title: ``,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-sass", {
+    resolve: 'gatsby-plugin-google-analytics',
+    options: {
+      "trackingId": "G-G0TJBL14ZY"
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }]
+};
